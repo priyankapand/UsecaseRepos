52e6abcffe76ceac4a6dec22768ebc51c2567e15
sonalvarma08@gmail.com <maysonpri>
2019-06-11 15:50:07 +0530

    Adding multiplication

---
+++ Imagica/src/main/java/MyProject/Imagica/Multiplication.java
@@ -0,0 +1,15 @@
+package MyProject.Imagica;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		a=20;
+		b=10;
+		c=a*b;
+		System.out.println(+c);
+		System.out.println("Multiplication is " +c);
+	}
+
+}
