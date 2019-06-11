498c5a70fb40d10c7d9d9e5254f428dd594e8d94
sonalvarma08@gmail.com <maysonpri>
2019-06-11 14:06:14 +0530

    Initial fetching

---
+++ maysonpri/src/main/java/maysonpri/maysonpri/App.java
@@ -0,0 +1,13 @@
+package maysonpri.maysonpri;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
