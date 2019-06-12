db797110f38a39d920dfac21e939ae275799439b
priyankapand <priyankapand@priyankapand-w8.cybage.com>
2019-06-12 12:23:01 +0530

    Commit feature1

---
+++ Imagica/src/main/java/MyProject/Imagica/Feature1.java
@@ -0,0 +1,15 @@
+package MyProject.Imagica;
+
+public class Feature1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		a=20;
+		b=10;
+		c=a-b;
+		System.out.println(+c);
+		System.out.println("Subtraction is " +c);
+	}
+
+}
