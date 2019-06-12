b9cb2d4cf01cadd2ff0bb1abe923d11475b50cd5
priyankapand <priyankapand@priyankapand-w8.cybage.com>
2019-06-12 12:21:01 +0530

    Commiting Both

---
+++ Imagica/src/main/java/MyProject/Imagica/AreaOfCircle.java
@@ -0,0 +1,16 @@
+package MyProject.Imagica;
+
+public class AreaOfCircle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	       double r = 2;
+	        
+	         System.out.println("Enter the radius:");
+	         
+	         double  area=(22*r*r)/7 ;
+	         System.out.println("Area of Circle is: " + area);   
+	}
+
+}
