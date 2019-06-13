d930f061c8d3f07eccc304d92426b607f714ab5c
priyankapand <priyankapand@priyankapand-w8.cybage.com>
2019-06-13 17:27:38 +0530

    Junit push

--- Imagica/src/main/java/MyProject/Imagica/Addition.java
+++ Imagica/src/main/java/MyProject/Imagica/Addition.java
@@ -1,15 +1,14 @@
 package MyProject.Imagica;
 
+
+
 public class Addition {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a,b,c;
-		a=10;
-		b=20;
-		c=a+b;
-		System.out.println(+c);
-		System.out.println("Addition is " +c);
+	
+	public int add(int a , int b)
+	{
+		return a+b;
 	}
-
+	
 }
+
