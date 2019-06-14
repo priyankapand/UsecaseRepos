a431ec9b14765cb97f2df9f52bfc4df0fdffe1fc
priyankapand <priyankapand@priyankapand-w8.cybage.com>
2019-06-14 15:25:55 +0530

    Comiting files

---
+++ FinalDemo/src/main/java/FinalDemo/FinalDemo/Addition.java
@@ -0,0 +1,11 @@
+package FinalDemo.FinalDemo;
+
+public class Addition {
+	
+public int add(int a , int b)
+	{
+			return a+b;
+		}
+		
+	}
+
---
+++ FinalDemo/src/test/java/FinalDemo/FinalDemo/AdditionTest.java
@@ -0,0 +1,21 @@
+package FinalDemo.FinalDemo;
+
+
+import org.junit.Assert.*;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AdditionTest{
+	@Test
+	public void testAddition()
+	{
+			
+	Addition ad =new Addition();
+	int result = ad.add(10, 20);
+// assert.AssertEquals(30,result);
+assertTrue("Error, random is too high", 30 == result);
+
+}
+}
